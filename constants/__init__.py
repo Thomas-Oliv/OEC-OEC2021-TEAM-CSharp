60fc9a7d854807217bdb299862accc0e8b91112c
Christian Traxler <traxler.lc@gmail.com>
2021-01-23 13:23:23 -0500

    updated multipliers

--- constants/__init__.py
+++ constants/__init__.py
@@ -1,5 +1,8 @@
 # students not expected to interact with teachers much closely
-TEACHER_STUDENT_MULTIPLIER = 0.8
+TEACHER_STUDENT_MULTIPLIER = 0.9
 
-# student ta interaction minimal?
-TA_STUDENT_MULTIPLIER = 1.0
+# student expected to interact with students moderately closely
+TA_STUDENT_MULTIPLIER = 1.1
+
+# teachers expected to interact with tas closely
+TEACHER_TA_MULTIPLIER = 1.5
