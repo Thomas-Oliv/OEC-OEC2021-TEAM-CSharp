89f92c98d91ab055f3b58088f9bf48b00742c1ff
ReneEML <rene.m.levesque@gmail.com>
2021-01-23 13:57:02 -0500

    extracurriculars

--- constants/__init__.py
+++ constants/__init__.py
@@ -1,3 +1,5 @@
+from Objects.Extracurricular import Extracurricular
+
 # students not expected to interact with teachers much closely
 TEACHER_STUDENT_MULTIPLIER = 0.9
 
@@ -6,3 +8,16 @@ TA_STUDENT_MULTIPLIER = 1.1
 
 # teachers expected to interact with tas closely
 TEACHER_TA_MULTIPLIER = 1.5
+
+EXTRACURRICULARS = {
+    'Board game club': [],
+    'Football': [],
+    'Soccer': [],
+    'Video Game Club': [],
+    'Band': [],
+    'Computer Science Club': [],
+    'Choir': [],
+    'Basketball': [],
+    'Badminton': [],
+    'Baseball': []
+}
