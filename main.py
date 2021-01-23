41ad04e9208ed968697ea36d61a9c7a564a66f05
Thomas-Oliv <drivemenuts766@gmail.com>
2021-01-23 12:52:38 -0500

    Added .xlsx to the project. Added Excel parsing to main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 
-import sys
-
+import sys,os
+from openpyxl import Workbook,load_workbook
 
 def main():
-    print('test')
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    directory = os.path.join(PROJECT_ROOT, 'OEC2021_-_School_Record_Book_.xlsx')
+    wb = load_workbook(read_only=True, filename=directory)
+    student_info = wb.sheetnames[0]
+    teacher_info = wb.sheetnames[1]
+    ta_info = wb.sheetnames[2]
+    infected_info = wb.sheetnames[3]
+    wb.close()
 
 if __name__ == "__main__":
     try:
