c87815c102fd1ad8d700a848c267eb5a8cd6c745
ReneEML <rene.m.levesque@gmail.com>
2021-01-23 14:47:47 -0500

    extracurricular activites

--- extracurricular_algo.py
+++ extracurricular_algo.py
@@ -1,6 +1,8 @@
 import constants
-from Objects.Student import Student
-from typing import List
+
+
+def computeNewChance(originalChance, newChance) -> float:
+    return min(1 - ((1 - originalChance) * (1 - newChance)), 1)
 
 
 def extra_calc(students):
@@ -8,3 +10,11 @@ def extra_calc(students):
     for student in students:
         for extra in student.extracurriculars:
             extracurriculars[extra].append(student)
+    for key in extracurriculars:
+        studentsExpectedToInfect = 0
+        curricularSize = len(extracurriculars[key])
+        for student in extracurriculars[key]:
+            studentsExpectedToInfect += student.chanceOfDisease * 3
+        for student in extracurriculars[key]:
+            chanceInfected = (studentsExpectedToInfect / curricularSize) * student.infectivity
+            student.chanceOfDisease = computeNewChance(student.chanceOfDisease, chanceInfected)
